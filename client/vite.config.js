c88ec587c90152ab053343ff9d9e6b70a20d5b80
CyberSam-11 <sahilpokharkar3@gmail.com>
2024-07-18 09:00:23 +0530

    Initial Commit

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      scenes: "/src/scenes",
+      state: "/src/state",
+    },
+  },
+})
